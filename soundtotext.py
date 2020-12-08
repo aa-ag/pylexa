22766bcc67417d22d2d63b43660971e0239db9c1
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-07 20:35:02 -0600

    microphone audio script

--- soundtotext.py
+++ soundtotext.py
@@ -10,13 +10,19 @@ r = speech_recognition.Recognizer()
 
 # open file
 
-with speech_recognition.AudioFile(file) as src:
-    # listen for data / load audio to memory
-    audio_data = r.record(src)
-    # convert from speech to text
-    txt_version = r.recognize_google(audio_data)
-    print(txt_version)
+# with speech_recognition.AudioFile(file) as src:
+#     # listen for data / load audio to memory
+#     audio_data = r.record(src)
+#     # convert from speech to text
+#     txt_version = r.recognize_google(audio_data)
+#     print(txt_version)
 
 # TO DO: 
 # - large audio files
 # - reading from microphone
+
+with speech_recognition.Microphone() as source:
+    a = r.record(source, duration=5)
+    print("Recognizing...")
+    t = r.recognize_google(a)
+    print(t)
